91d08f2b46844e94e556183aff801b30d964da9e
JarlShadown <miguel.payran.del.angel@outlook.com>
2022-03-12 11:44:56 -0800

    header finish

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import {Papper, Typography, useMediaQuery} from "@material-ui/core";
+import MyLocationOutlinedIcon from "@material-ui/icons/LocalActivityOutlined";
+import Rating from "@material-ui/lab";
+import useStyles from "./styles";
+
+const Map = () => {
+    return(
+        <h1>Map</h1>
+    )
+}
+
+export default Map;
